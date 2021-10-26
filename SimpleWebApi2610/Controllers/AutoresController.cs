26b9516bcde99899504a259d29247edd024623e6
Gerson Azabache <gerson_dunk@hotmail.com>
2021-10-26 10:32:39 -0500

    autor controller completed

--- SimpleWebApi2610/Controllers/AutoresController.cs
+++ SimpleWebApi2610/Controllers/AutoresController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SimpleWebApi2610.Entidades;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,14 @@ namespace SimpleWebApi2610.Controllers
     [ApiController]
     [Route("api/[controller]")]
     public class AutoresController : ControllerBase
-    {        
+    {
+        private readonly ApplicationDBContext context;
+
+        public AutoresController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
         /// <summary>
         /// Este método trae todos los autores
         /// </summary>
@@ -21,12 +29,50 @@ namespace SimpleWebApi2610.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Autor>>> Get()
         {
-            return new List<Autor>()
-            {
-                new Autor{ Id = 1, Nombre = "Gerson" },
-                new Autor{ Id = 2, Nombre = "Luis" },
-                new Autor{ Id = 3, Nombre = "Antonia" }
-            };
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            await context.Autores.AddAsync(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, Autor autor)
+        {
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+                return NotFound("no se encontró el autor que se quiere actualizar");
+
+            if (id != autor.Id)
+                return BadRequest("los ids no coinciden");
+
+            context.Autores.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        /// <summary>
+        /// Este método trae todos los autores
+        /// </summary>
+        /// <returns>Puros autores</returns>
+        /// <remarks>Este es un remark para información ampliada de autores de libros comunes y conocidos.</remarks>
+        /// <response code="200">Todo bien eh</response>
+        /// <response code="400">La liaste ahsss</response>   
+        /// <response code="404">No existe</response>   
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+                return NotFound($"El autor con id {id} que se quiere eliminar no existe");
+
+            context.Autores.Remove(new Autor { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
