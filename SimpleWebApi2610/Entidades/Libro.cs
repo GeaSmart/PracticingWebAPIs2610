a2452e44bb3d8b34a2a9dd0405985054b155efda
Gerson Azabache <gerson_dunk@hotmail.com>
2021-10-27 16:00:05 -0500

    validations

--- SimpleWebApi2610/Entidades/Libro.cs
+++ SimpleWebApi2610/Entidades/Libro.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SimpleWebApi2610.Validaciones;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +11,29 @@ namespace SimpleWebApi2610.Entidades
     public class Libro
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [StringLength(maximumLength:15,MinimumLength = 5,ErrorMessage = "El campo {0} debe tener una longitud entre {2} y {1} caracteres")]
+        [PrimeraLetraMayuscula]
         public string Titulo { get; set; }
+
+        [Range(1,500)]
+        [NotMapped] //hace que no se tenga que corresponder con una tabla en la bd
+        public int NumeroPaginas { get; set; }
+
+        [NotMapped]
+        [CreditCard]
+        public string TarjetaCredito { get; set; }
+
+        [NotMapped]
+        [Url]
+        public string Url { get; set; }
+
+        [NotMapped]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+
         public int AutorId { get; set; }
 
         //Propiedad de navegacion
